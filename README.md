3a10461d8872a6aff21ac7fd436df6f69bc757a4
Matt Campbell <mecampbell25@gmail.com>
2013-10-16 10:43:07 -0400

    basic setup working

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Ruby Project Setup Gem
 A gem to setup a folder structure for a Ruby project.
+
+0.0.0
+
+Creates basic project directory structure with rspec --init included
+
+0.0.1
+
+Want user to pass flags like:
+
+-s creates _sites and other HTML dirs
+
+User passes in model names
--- test.rb
+++ test.rb
@@ -1,12 +1,47 @@
 require 'fileutils'
 
-class Setup
+class Aspen
 
-  def self.run(*args)
-    FileUtils.mkdir_p("#{args[0]}", :verbose => true)
-    FileUtils.mkdir_p("#{args[0]}/", :verbose => true)
+  # Aspen.run("roman numerals -s")
+  # Aspen.run("roman_numerals -s")
+
+  def self.method_missing(method, *args, &block)
+    puts "Invalid command - try again."
   end
 
+  def self.run(*args)
+    @@root_name = sterilize_project_name(args[0])
+    return if @@root_name == nil
+    FileUtils.mkdir_p("#{@@root_name}", :verbose => true)
+    FileUtils.mkdir_p("#{@@root_name}/lib/models", :verbose => true) # models go here!
+    FileUtils.mkdir_p("#{@@root_name}/lib/concerns", :verbose => true) # modules go here!
+    FileUtils.mkdir_p("#{@@root_name}/bin", :verbose => true)
+    #FileUtils.mkdir_p("#{@@root_name}/spec", :verbose => true)
+    FileUtils.mkdir_p("#{@@root_name}/config", :verbose => true)
+    rspec_init
+    make_files
+  end
+
+  def self.rspec_init
+    current = FileUtils.pwd()
+    FileUtils.cd("#{@@root_name}")
+    system('rspec --init')
+    FileUtils.cd(current)
+  end
+
+  def self.make_files
+    FileUtils.touch("#{@@root_name}/README.md") #README
+    FileUtils.touch("#{@@root_name}/config/environment.rb") #environment
+  end
+
+  def self.sterilize_project_name(string)
+    if string.match(/[^\w|\s|\-|.]|\d/)
+      puts "Invalid project name"
+    else
+      string.strip.downcase.gsub(" ", "-").gsub("_", "-")
+    end
+  end
 end
 
-Setup.run("Project")
+Aspen.run("the french kinda suck é &")
+Aspen.run("good name")
