e6a8e2b577c670245f3d22402970626225a0b3dc
Logan Hasson <logan.hasson@gmail.com>
2013-10-17 18:26:52 -0400

    Ignore templates dir in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,12 @@ require 'rspec'
 require 'pry'
 
 AspenRoot = File.expand_path(File.dirname(__FILE__)).gsub("/config", "")
-#binding.pry
-
 
 def get_reqs(current_dir)
   Dir.foreach(current_dir) do |file|
     next if file.start_with?('.') || file.start_with?("environment.rb")
     if File.directory?("#{current_dir}/#{file}")
+      next if file.start_with?("templates")
       get_reqs("#{current_dir}/#{file}")
     else
       require_relative "#{current_dir}/#{file}" if file.end_with?(".rb")
