3128974f508f747a6eba334680a9a01b8d54f94a
Matt Campbell <mecampbell25@gmail.com>
2013-10-20 18:00:31 -0400

    Moving parsing methods to Parsable module

--- lib/models/aspen.rb
+++ lib/models/aspen.rb
@@ -1,42 +1,28 @@
-require_relative '../config/environment'
+require_relative '../../config/environment'
 
 class Aspen
 
   extend Parsable
 
   def self.directories
-    @@directories
+    @directories
   end
 
   def self.run
-    @@root_name = sterilize_project_name(@@args.leftovers)
     load_template
-    @@directories = AspenTemplate.tree(TEMPLATE)
+    @directories = AspenTemplate.tree(TEMPLATE)
     project_init
   end
 
   def self.gem_init
     current = FileUtils.pwd()
-    FileUtils.cd("#{@@root_name}")
+    FileUtils.cd("#{@root_name}")
     yield 
     FileUtils.cd(current)
   end
 
   def self.git
     gem_init { Git.init }
-  end
-
-  def self.load_template
-    if @@opts[:template] == nil
-      require_relative "#{AspenRoot}/lib/templates/default.rb"
-    else
-      Dir.foreach("#{AspenRoot}/lib/templates") do |file|
-        if file.start_with?("#{@@opts[:template].downcase.strip}")
-          require_relative "#{AspenRoot}/lib/templates/#{file}"
-          break
-        end
-      end
-    end
   end
 
   def self.rspec_init
@@ -46,38 +32,29 @@ class Aspen
   def self.project_init
     directory_init
     rspec_init
-    make_files if @@opts[:template] == nil || @@opts[:template] == "default"
+    make_files if @opts[:template] == nil || @opts[:template] == "default"
     successful_creation
     write_to_files
     process_flags
   end
 
   def self.directory_init
-    self.directories.each {|dir| FileUtils.mkdir_p("#{@@root_name}#{dir}", :verbose => true)}
+    self.directories.each {|dir| FileUtils.mkdir_p("#{@root_name}#{dir}", :verbose => true)}
   end
 
   def self.make_files
-    FileUtils.touch("#{@@root_name}/README.md")
-    FileUtils.touch("#{@@root_name}/config/environment.rb")
+    FileUtils.touch("#{@root_name}/README.md")
+    FileUtils.touch("#{@root_name}/config/environment.rb")
   end
 
   def self.write_to_files
-    File.open("#{@@root_name}/README.md", "w+") do |f|
-      f << "###{@@project_name}\n*thanks for using aspen - happy coding!*"
+    File.open("#{@root_name}/README.md", "w+") do |f|
+      f << "###{@project_name}\n*thanks for using aspen - happy coding!*"
     end
   end
 
   def self.successful_creation
-    puts "\nProject successfully created at:\n\t#{Dir.pwd}/#{@@root_name}\n"
+    puts "\nProject successfully created at:\n\t#{Dir.pwd}/#{@root_name}\n"
   end
 
-  def self.sterilize_project_name(name)
-    name = name.join(" ")
-    if name.match(/^[a-z]/i)
-      @@project_name = name
-      name.strip.downcase.gsub(/[\s\_]/,"-")
-    else
-      puts "Invalid project name: #{name}"
-    end
-  end
 end
--- lib/models/concerns/parsable.rb
+++ lib/models/concerns/parsable.rb
@@ -1,8 +1,8 @@
 module Parsable
 
-  @@mono_flags = [:git]
+  @mono_flags = [:git]
 
-  @@args = Trollop::Parser.new do
+  @args = Trollop::Parser.new do
     version "aspen-0.2.2"
     banner <<-EOS
  -----------------------------------------------------------------------------
@@ -21,18 +21,44 @@ EOS
     opt :template, "Specify a template structure", :type => :string                   
   end
 
-  @@opts = Trollop::with_standard_exception_handling @@args do
-    temp_args = @@args.parse ARGV
-    raise Trollop::HelpNeeded if ARGV.empty? || @@args.leftovers.size == 0
+  @opts = Trollop::with_standard_exception_handling @args do
+    temp_args = @args.parse ARGV
+    raise Trollop::HelpNeeded if ARGV.empty? || @args.leftovers.size == 0
     temp_args
   end
 
+  def self.sterilize_project_name(name)
+    name = name.join(" ")
+    if name.match(/^[a-z]/i)
+      @project_name = name
+      name.strip.downcase.gsub(/[\s\_]/,"-")
+    else
+      puts "Invalid project name: #{name}"
+    end
+  end
+
+  @root_name = sterilize_project_name(@args.leftovers)
+
   def self.process_flags
-    @@mono_flags.each do |flag|
-      if @@opts[flag] == true
+    @mono_flags.each do |flag|
+      if @opts[flag] == true
         self.send(flag)
       end
     end
   end
+
+  def self.load_template
+    if @opts[:template] == nil
+      require_relative "#{AspenRoot}/lib/templates/default.rb"
+    else
+      Dir.foreach("#{AspenRoot}/lib/templates") do |file|
+        if file.start_with?("#{@opts[:template].downcase.strip}")
+          require_relative "#{AspenRoot}/lib/templates/#{file}"
+          break
+        end
+      end
+    end
+  end
+  
 
 end
